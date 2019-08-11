b0a6a324416f731804629a69b738cc5200645bef
takenokoya <takuya1002@gmail.com>
2019-08-11 17:16:07 +0900

    modify outlet

--- SummerCampNews/MainViewController.swift
+++ SummerCampNews/MainViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import XLPagerTabStrip
+import WebKit
 
 class MainViewController: ButtonBarPagerTabStripViewController {
     
--- SummerCampNews/NewsViewController.swift
+++ SummerCampNews/NewsViewController.swift
@@ -12,14 +12,18 @@ import WebKit
 
 class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDelegate, UITableViewDataSource, WKNavigationDelegate, XMLParserDelegate {
 
+    @IBOutlet weak var UIToolbar: UIToolbar!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var cancel: UIBarButtonItem!
+    @IBOutlet weak var nextPage: UIBarButtonItem!
+    @IBOutlet weak var backPage: UIBarButtonItem!
+    @IBOutlet weak var refreshPage: UIBarButtonItem!
+    
     //TableViewのインスタンス取得
     var tableView: UITableView = UITableView()
     
     //XMLParserのインスタンスを取得
     var parser = XMLParser()
-    
-    @IBOutlet weak var UIToolbar: UIToolbar!
-    @IBOutlet weak var webView: WKWebView!
     
     // urlを受け取る
     var url: String = ""
@@ -33,6 +37,16 @@ class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDe
     
     //セルの数
     var articles = NSMutableArray()
+    
+    // xmlファイルに解析をかけた内容
+    var elements = NSMutableDictionary()
+    // xmlファイルのタグ情報
+    var element = String()
+    // xmlファイルのタイトル情報
+    var titleString = NSMutableString()
+    //xmlファイルのリンク情報
+    var linkString = NSMutableString()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
@@ -44,9 +58,11 @@ class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDe
         cell.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         //記事テキストサイズとフォント
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        cell.textLabel?.text = (articles[indexPath.row] as AnyObject).value(forKey: "title") as? String
         cell.textLabel?.textColor = UIColor.black
         //記事urlサイズとフォント
         cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.detailTextLabel?.text = (articles[indexPath.row] as AnyObject).value(forKey: "link") as? String
         cell.detailTextLabel?.textColor = UIColor.gray
         
         return cell
@@ -54,24 +70,33 @@ class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDe
     
     //セルをタップしたときの処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        // webviewを表示する
+        let linkURL = ((articles[indexPath.row] as AnyObject).value(forKey: "link") as? String)?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let urlStr = (linkURL?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed))!
+        guard let url = URL(string: urlStr) else {
+            return
+        }
+        let urlRequest = NSURLRequest(url: url)
+        webView.load(urlRequest as URLRequest)
     }
     
     //
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         tableView.isHidden = true
         UIToolbar.isHidden = false
-        webView.isHidden = false
+        self.webView.isHidden = false
     }
     
     @IBAction func cancel(_ sender: Any) {
-        
+        tableView.isHidden = false
+        webView.isHidden = true
+        UIToolbar.isHidden = true
     }
     
-    @IBAction func backpage(_ sender: Any) {
+    @IBAction func backPage(_ sender: Any) {
         webView.goBack()
     }
-    @IBAction func nextpage(_ sender: Any) {
+    @IBAction func nextPage(_ sender: Any) {
         webView.goForward()
     }
     @IBAction func refreshPage(_ sender: Any) {
@@ -87,19 +112,18 @@ class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDe
         //navigationGeligateとの接続
         webView.navigationDelegate = self
         
-        //parserとの接続
-        parser.delegate = self
-        
         //デリゲートとの接続
         tableView.delegate = self
         tableView.dataSource = self
         
-        tableView.frame = CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.height - 50.0)
+        tableView.frame = CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.height - 50)
         self.view.addSubview(tableView)
 
         //最初はTableViewを表示したいためtoolbarとwebviewを隠しておく
         UIToolbar.isHidden = true
         webView.isHidden = true
+        
+        parseUrl()
     }
     
     // URLを解析する
@@ -110,9 +134,51 @@ class NewsViewController: UIViewController, IndicatorInfoProvider, UITableViewDe
         parser = XMLParser(contentsOf: urlToSend)!
         // 記事情報を初期化
         articles = []
-        //解析の実行
+        // parserとの接続
+        parser.delegate = self
+        // 解析の実行
         parser.parse()
         // TableViewのリロード
         tableView.reloadData()
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        element = elementName
+        
+        // エレメントにタイトルが入ってきたら
+        if element == "item" {
+            // 初期化
+            elements = [:]
+            titleString = ""
+            linkString = ""
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if element == "title" {
+            // stringにタイトルが入っているのでappend
+            titleString.append(string)
+        } else if element == "link" {
+            linkString.append(string)
+        }
+    }
+
+    // 終了タグを見つけた時
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        // アイテムという要素の中にあるなら、
+        if elementName == "item" {
+            // titleString,linkStringの中身が空でないなら
+            if titleString != "" {
+                // elementsに"title"、"Link"というキー値を付与しながらtitleString,linkStringをセット
+                elements.setObject(titleString, forKey: "title" as NSCopying)
+            }
+            if linkString != "" {
+                elements.setObject(linkString, forKey: "link" as NSCopying)
+            }
+            
+            // articlesの中にelementsを入れる
+            articles.add(elements)
+        }
     }
 }
