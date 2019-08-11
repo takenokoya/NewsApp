3af956082d2ec966a1fc8175d97be706137a7537
takenokoya <takuya1002@gmail.com>
2019-08-11 09:11:57 +0900

    Initial Commit

---
+++ SummerCampNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SummerCampNews
+//
+//  Created by 坂口卓也 on 2019/08/11.
+//  Copyright © 2019 坂口卓也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
